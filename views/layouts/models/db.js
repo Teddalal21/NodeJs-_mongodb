6535ccf7abe302e63923627a165376bf2ac3421c
Teddalal21 <tadessetsega280@gmail.com>
2023-03-12 14:05:14 +0300

    mongodb connection updated successfully

--- views/layouts/models/db.js
+++ views/layouts/models/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
 
-// const connecDB = () => {
-//     mongoose.connect('mongodb://localhost/mydatabase', {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//       })
-//         .then(() => console.log('MongoDB connected'))
-//         .catch((err) => console.log(err));
-// }
+const connecDB = () => {
+    mongoose.connect('mongodb://localhost/mydatabase', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+        .then(() => console.log('MongoDB connected'))
+        .catch((err) => console.log(err));
+}
 
-// module.exports = connecDB
+module.exports = connecDB
 
-// require('./student_model.js')
+require('./student_model.js')
